11aff2a11dfc6a35975b4b134e612a29ff8b422c
Cris Moen <cris.moen@runtosolve.com>
2022-04-26 15:30:29 -0400

    add cross-section normal functions

--- src/Tools.jl
+++ src/Tools.jl
@@ -87,4 +87,119 @@ function generate_straight_line_segments(cross_section_nodes, corners, n)
 
 end
 
+
+
+
+# calculate surface normals for each line segment in a 2D cross-section
+function surface_normals(xcoords, ycoords, closed_or_open)
+
+
+    if closed_or_open == 0
+        numel = length(xcoords)   #closed
+    elseif closed_or_open ==1
+        numel = length(xcoords) - 1  #open
+    end
+
+    unitnormals = zeros(Float64, (numel, 2))
+
+    for i=1:numel
+
+        if (i == numel) & (closed_or_open == 0)   #for tubes
+            pointA = Point(xcoords[i], ycoords[i])
+            pointB = Point(xcoords[1], ycoords[1])
+        else
+            pointA = Point(xcoords[i], ycoords[i])
+            pointB = Point(xcoords[i + 1], ycoords[i + 1])
+        end
+
+        dx = pointB.x - pointA.x
+        dy = pointB.y - pointA.y
+
+        normAB = norm([dx, dy])
+
+        unitnormals[i, :] = [-dy, dx] / normAB
+
+        if unitnormals[i,1] == -0.0
+            unitnormals[i,1]= 0.0
+        end
+
+        if unitnormals[i,2] == -0.0
+            unitnormals[i,2]= 0.0
+        end
+
+    end
+
+    return unitnormals
+
+end
+
+
+# calculate average unit normals at each node in a 2D cross-section from element unit normals
+function avg_node_normals(unitnormals, closed_or_open)
+
+    if closed_or_open == 0
+        numnodes = size(unitnormals)[1]
+        numel = size(unitnormals)[1]
+    elseif closed_or_open ==1
+        numnodes = size(unitnormals)[1]+1
+        numel = size(unitnormals)[1]
+    end
+
+    nodenormals = zeros(Float64, (numnodes, 2))
+
+    for i=1:numnodes
+
+        if (i == 1) & (closed_or_open == 0)  # where nodes meet in the tube
+            nodenormals[i, :] = mean(unitnormals[[numel, 1], :], dims=1)
+        elseif (i != 1) & (closed_or_open == 0)  #tube
+            nodenormals[i, :] = mean(unitnormals[i-1:i, :], dims=1)
+        elseif (i == 1) & (closed_or_open == 1)  #open, first node is element norm
+            nodenormals[i, :] = unitnormals[i, :]
+        elseif (i != 1) & (i != numnodes) & (closed_or_open == 1)  #open
+            nodenormals[i, :] = mean(unitnormals[i-1:i, :], dims=1)
+        elseif (i == numnodes) & (closed_or_open == 1)  #open, last node is element norm
+            nodenormals[i, :] = unitnormals[i-1, :]
+
+        end
+
+        #make sure unit normal always = 1.0
+        unitnorm = norm(nodenormals[i, :])
+        if unitnorm < 0.99
+            scale = 1.0/unitnorm
+            nodenormals[i,:] = scale .* nodenormals[i,:]
+        end
+
+    end
+
+    return nodenormals
+
+end
+
+
+function xycoords_along_normal(xcoords, ycoords, nodenormals, Δ)
+
+    numnodes = size(xcoords)[1]
+    xcoords_normal = []
+    ycoords_normal = []
+
+    for i=1:numnodes
+
+        if i == 1
+
+            xcoords_normal = xcoords[i] + nodenormals[i, 1] * Δ
+            ycoords_normal = ycoords[i] + nodenormals[i, 2] * Δ
+
+        else
+
+            xcoords_normal = [xcoords_normal; (xcoords[i] + nodenormals[i, 1] * Δ)]
+            ycoords_normal = [ycoords_normal; (ycoords[i] + nodenormals[i, 2] * Δ)]
+
+        end
+
+    end
+
+    return xcoords_normal, ycoords_normal
+
+end
+
 end #module
--- test/stiffened_C_section.jl
+++ test/stiffened_C_section.jl
@@ -1,5 +1,4 @@
 using CrossSection
-using LinesCurves
 
 
 L = [0.75, 2.0, 4.0, 0.75]
@@ -7,10 +6,6 @@ L = [0.75, 2.0, 4.0, 0.75]
 n_radius = [3, 3, 3]
 n = [4, 4, 4, 4]
 r = 0.09 * ones(Float64, 3)
-
-start_node = [1.0, 3.0]
-cdm = LinesCurves.transform_vector(L[1], start_node, θ[1])
-
 
 cross_section = CrossSection.generate_open(L, θ, r, n, n_radius)
 
