f2d5ad75c79edcdf1b004a0094beae6b211f1f01
Cris Moen <cris.moen@runtosolve.com>
2022-05-05 15:36:37 -0400

    add node normal test

---
+++ test/node_normal.jl
@@ -0,0 +1,26 @@
+using CrossSection
+
+
+d = 1.0
+b = 3.5
+h = 10.0
+r_inside = 0.1863
+t = 0.1242 
+
+
+L = [d, b, h, b, d]
+θ = [π/2, π, 3π/2, 0.0, π/2]
+n = [4, 4, 4, 4, 4]
+n_radius = [3, 3, 3, 3]
+r = r_inside * ones(Float64, length(n_radius))  .+ t  #outside 
+
+cross_section = CrossSection.generate_open(L, θ, r, n, n_radius)
+
+using Plots
+plot(cross_section[:, 1], cross_section[:, 2], markershape = :o, aspect_ratio=:equal)
+
+unit_node_normals = CrossSection.Tools.calculate_cross_section_unit_node_normals(cross_section)
+x_center, y_center = CrossSection.Tools.get_coords_along_node_normals(cross_section[:,1], cross_section[:, 2], unit_node_normals, -t/2)
+
+plot!(x_center, y_center, markershape = :o, aspect_ratio=:equal)
+
